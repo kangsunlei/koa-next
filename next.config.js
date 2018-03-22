e18af5913971cf73c8836ca85f8ca7287b338898
kangsunlei <kangsunlei@yifangyun.com>
2018-03-22 17:15:50 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-const withSass = require('@zeit/next-sass')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const { ANALYZE } = process.env
+const withSass = require('@zeit/next-sass');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const { ANALYZE } = process.env;
 
 module.exports = withSass({
-	webpack: function (config) {
-		if (ANALYZE) {
-			config.plugins.push(new BundleAnalyzerPlugin({
-				analyzerMode: 'server',
-				analyzerPort: 8888,
-				openAnalyzer: true
-			}))
-		}
+    webpack(config) {
+        if (ANALYZE) {
+            config.plugins.push(new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                analyzerPort: 8888,
+                openAnalyzer: true
+            }));
+        }
 
-		return config
-	}
+        return config;
+    }
 });
